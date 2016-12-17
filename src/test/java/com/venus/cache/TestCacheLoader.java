82ee04bac66f7e77e8b52880638337ebe12c55c3
liuzy <liuziyingbeidou@163.com>
2016-12-17 17:44:47 +0800

    Guava 实现缓存 Callable and CacheLoader）

--- src/test/java/com/venus/cache/TestCacheLoader.java
+++ src/test/java/com/venus/cache/TestCacheLoader.java
@@ -2,19 +2,18 @@ package com.venus.cache;
 
 import org.junit.Test;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by liuzy on 2016/12/17 0017.
  */
-public class TestCacheCallable {
-    @Test
-    public void CacheCallableTest() throws ExecutionException, InterruptedException {
-        for (int i = 0; i < 20; i++){
-            Object consult = CacheCallable.get("test1","Test DB");
+public class TestCacheLoader {
 
+    @Test
+    public void CacheLoaderTest() throws ExecutionException, InterruptedException {
+        for (int i = 0; i < 20; i++){
+            Object consult = CacheLoading.get("tesst-0","database");
             System.out.println(consult);
             TimeUnit.SECONDS.sleep(1);
         }
